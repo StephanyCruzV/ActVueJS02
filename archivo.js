a622bc60573f36611fd5f545e16d3457bcb9fa42
StephanyCruzV <101074164+StephanyCruzV@users.noreply.github.com>
2022-04-01 18:16:48 -0600

    actualización de archivos

---
+++ archivo.js
@@ -0,0 +1,18 @@
+Vue.createApp({
+    data() {
+      return {
+        information: {
+          name: "=================== LA FLDSMDFR - VACATION TRIP ===================",
+          id: "<< Página para ver que actividades se pueden hacer en vacaciones >>"
+        },
+        interests: ["Camping", "Water Parks", "Beach", "Theme Park", "Waterfalls"],
+        
+        // variable para mensaje con v-model 
+        message:'',
+        
+        // variable para if con boton
+        done:false,
+      
+      };
+    }
+  }).mount("#app");
